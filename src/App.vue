a2b8b7b46d9be19221a69cab3907bfa8e2075f6d
collie <2541474139@qq.com>
2023-01-17 15:53:51 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>app</div>
+  <First />
+</template>
+
+<script>
+import First from './components/First.vue'
+
+export default {
+  name: 'App',
+  components: {
+    First
+  }
+}
+</script>
---
+++ src/components/First.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    这是我的第一个 vue3 组件
+  </div>
+</template>
+
