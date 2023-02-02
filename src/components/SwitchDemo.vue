8fe7883af397d0327d39db9c583b5a0244484320
collie <2541474139@qq.com>
2023-02-02 18:36:39 +0800

    基本实现 Switch 的功能

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
-    <Switch />
+    <Switch v-model:value="bool" />
   </div>
 </template>
 
 <script lang="ts">
 import Switch from "../lib/Switch.vue";
+import { ref } from "vue";
 
 export default {
   name: "SwitchDemo",
   components: {
     Switch,
+  },
+  setup() {
+    const bool = ref(false);
+    return { bool };
   },
 };
 </script>
