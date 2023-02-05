e06bc9803965d5d12c505b218172532bee29e0a3
collie <2541474139@qq.com>
2023-02-05 20:16:50 +0800

    创建Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>ButtonDemo组件</div>
+  <div>Button示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button>你好</Button>
+  </div>
 </template>
 
 <script lang="ts">
+import Button from "../lib/Button.vue";
+
 export default {
   name: "ButtonDemo",
+  components: {
+    Button,
+  },
 };
 </script>
 
