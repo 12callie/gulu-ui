96f3f1d0d1a051a25c8d65a675487a86cfd01d71
collie <2541474139@qq.com>
2023-02-09 16:45:24 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>DialogDemo组件</div>
+  <div>Dialog 示例</div>
+  <h1>示例1</h1>
+  <Dialog />
 </template>
 
 <script lang="ts">
+import Dialog from "../lib/Dialog.vue";
+
 export default {
   name: "DialogDemo",
+  components: {
+    Dialog,
+  },
 };
 </script>
 
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+      <header>标题</header>
+      <main>
+        <p>第一行字</p>
+        <p>第二行字</p>
+      </main>
+      <footer>
+        <Button>取消</Button>
+        <Button>确定</Button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "./Button.vue";
+
+export default {
+  name: "Dialog",
+  components: {
+    Button,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
