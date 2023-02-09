84178b658f688aef7b76870409a8eb1a159952f6
collie <2541474139@qq.com>
2023-02-09 17:24:51 +0800

    完成Dialog样式

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-    <div class="dialog-overlay"></div>
-    <div class="dialog-wrapper">
-      <header>标题</header>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-wrapper">
+    <div class="gulu-dialog">
+      <header>
+        标题
+        <span class="gulu-dialog-close">
+          <svg class="icon">
+            <use xlink:href="#icon-close"></use>
+          </svg>
+        </span>
+      </header>
       <main>
         <p>第一行字</p>
         <p>第二行字</p>
       </main>
       <footer>
         <Button>取消</Button>
-        <Button>确定</Button>
+        <Button level="main">确定</Button>
       </footer>
     </div>
   </div>
@@ -26,4 +33,55 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+.gulu-dialog {
+  background-color: #fff;
+  min-width: 15em;
+  max-width: 90%;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.6);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+
+  > main {
+    padding: 12px 16px;
+  }
+
+  > footer {
+    padding: 12px 16px;
+    border-top: 1px solid $border-color;
+    text-align: right;
+  }
+
+  &-close {
+    cursor: pointer;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import "./index.scss";
 import "./lib/guluLib.scss";
 import { createApp } from "vue";
 import App from "./App.vue";
-import "//at.alicdn.com/t/c/font_3874450_z1ykjug2pnq.js";
+import "//at.alicdn.com/t/c/font_3874450_uovaandht2m.js";
 import { router } from "./router";
 
 const app = createApp(App);
