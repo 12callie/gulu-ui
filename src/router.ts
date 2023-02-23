2facbc3d7da3e88201f6bd71fb48cd834d98a1bd
collie <2541474139@qq.com>
2023-02-23 16:53:24 +0800

    优化重复代码

--- src/router.ts
+++ src/router.ts
@@ -6,9 +6,13 @@ import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
 import DocDemo from "./components/DocDemo.vue";
-import Intro from "./views/Intro.vue";
-import GetStarted from "./views/GetStarted.vue";
-import Install from "./views/Install.vue";
+import Markdown from "./components/Markdown.vue";
+
+const md = (filename) => {
+  return h(Markdown, { path: `../markdown/${filename}.md`, key: filename });
+};
+
+import { h } from "vue";
 
 const history = createWebHistory();
 export const router = createRouter({
@@ -20,9 +24,18 @@ export const router = createRouter({
       component: Doc,
       children: [
         { path: "", component: DocDemo },
-        { path: "intro", component: Intro },
-        { path: "get-started", component: GetStarted },
-        { path: "install", component: Install },
+        {
+          path: "intro",
+          component: md("intro"),
+        },
+        {
+          path: "get-started",
+          component: md("getStarted"),
+        },
+        {
+          path: "install",
+          component: md("install"),
+        },
         { path: "switch", component: SwitchDemo },
         { path: "button", component: ButtonDemo },
         { path: "dialog", component: DialogDemo },
