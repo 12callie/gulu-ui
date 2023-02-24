35a0982161ae146a46f51f1c725082961f24470b
collie <2541474139@qq.com>
2023-02-24 18:38:10 +0800

    实现代码高亮

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,16 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre v-text="Switch1Demo.__sourceCode"></pre>
+        <pre
+          class="language-css"
+          v-html="
+            Prism.highlight(
+              Switch1Demo.__sourceCode,
+              Prism.languages.html,
+              'html'
+            )
+          "
+        />
       </div>
     </div>
     <div class="demo">
@@ -22,7 +31,16 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{ Switch2Demo.__sourceCode }}</pre>
+        <pre
+          class="language-css"
+          v-html="
+            Prism.highlight(
+              Switch2Demo.__sourceCode,
+              Prism.languages.html,
+              'html'
+            )
+          "
+        />
       </div>
     </div>
   </div>
@@ -33,6 +51,7 @@ import Button from "../lib/Button.vue";
 import { ref } from "vue";
 import Switch1Demo from "./Switch1.demo.vue";
 import Switch2Demo from "./Switch2.demo.vue";
+import Prism from "prismjs";
 
 export default {
   components: {
@@ -44,10 +63,15 @@ export default {
       bool,
       Switch1Demo,
       Switch2Demo,
+      Prism,
     };
   },
 };
 </script>
+
+<style>
+@import "prismjs/themes/prism.css";
+</style>
 
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
