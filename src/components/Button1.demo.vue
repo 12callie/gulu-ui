d2bdb2577700acfe82a8b3002defa3c774b33e51
collie <2541474139@qq.com>
2023-02-27 15:22:56 +0800

    改造 Button Demo

---
+++ src/components/Button1.demo.vue
@@ -0,0 +1,17 @@
+<demo>常规用法</demo>
+<template>
+  <Button>默认</Button>
+  <Button theme="button">默认</Button>
+  <Button theme="text">文本</Button>
+  <Button theme="link">链接</Button>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
---
+++ src/components/Button2.demo.vue
@@ -0,0 +1,30 @@
+<demo>支持 size</demo>
+<template>
+  <div>
+    <div>
+      <Button size="large">大</Button>
+      <Button size="normal">常规</Button>
+      <Button size="small">小</Button>
+    </div>
+    <div>
+      <Button theme="text" size="large">大</Button>
+      <Button theme="text">常规</Button>
+      <Button theme="text" size="small">小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="large">大</Button>
+      <Button theme="link">常规</Button>
+      <Button theme="link" size="small">小</Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+
+export default {
+  components: {
+    Button,
+  },
+};
+</script>
