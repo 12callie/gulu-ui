45dc10eec5be65812ee83c4764b40fbb5f4790fc
collie <2541474139@qq.com>
2023-02-27 15:48:07 +0800

    改造 Tabs demo

---
+++ src/components/Tabs1.demo.vue
@@ -0,0 +1,25 @@
+<demo>常规用法</demo>
+<template>
+  <Tabs v-model:selected="x">
+    <Tab title="导航">内容一</Tab>
+    <Tab title="导航导航">内容二</Tab>
+    <Tab title="导航导航导航">内容三</Tab>
+  </Tabs>
+</template>
+
+<script lang="ts">
+import Tabs from "../lib/Tabs.vue";
+import Tab from "../lib/Tab.vue";
+import { ref } from "vue";
+
+export default {
+  components: {
+    Tabs,
+    Tab,
+  },
+  setup() {
+    const x = ref("导航");
+    return { x };
+  },
+};
+</script>
--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,29 +1,8 @@
 <template>
-  <div>Tabs示例</div>
-  <h1>示例1</h1>
-  <Tabs v-model:selected="x">
-    <Tab title="导航">内容一</Tab>
-    <Tab title="导航导航">内容二</Tab>
-    <Tab title="导航导航导航">内容三</Tab>
-  </Tabs>
+  <h1>Tabs组件示例</h1>
+  <Demo :component="Tabs1Demo" />
 </template>
-
-<script lang="ts">
-import Tabs from "../lib/Tabs.vue";
-import Tab from "../lib/Tab.vue";
-import { ref } from "vue";
-
-export default {
-  name: "TabsDemo",
-  components: {
-    Tabs,
-    Tab,
-  },
-  setup() {
-    const x = ref("导航");
-    return { x };
-  },
-};
+<script lang="ts" setup>
+import Demo from "./Demo.vue";
+import Tabs1Demo from "./Tabs1.demo.vue";
 </script>
-
-<style lang="scss" scoped></style>
